269ed8fdc42c2342488dda5030f96e5935299011
Gustav Palmqvist <gustavp@gpa-robotics.com>
2020-12-12 01:08:42 +0100

    Added logarithmic units

--- src/lib.rs
+++ src/lib.rs
@@ -403,7 +403,8 @@ pub enum ConstantOp {
 ///
 /// [units]: https://jcgm.bipm.org/vim/en/1.13.html
 /// [factor]: https://jcgm.bipm.org/vim/en/1.24.html
-pub trait Conversion<V> {
+pub trait Conversion<V> where
+    V: Conversion<V, T = Self::T> {
     /// Conversion factor type specific to the underlying storage type.
     type T: ConversionFactor<V>;
 
@@ -425,6 +426,28 @@ pub trait Conversion<V> {
     #[allow(unused_variables)]
     fn constant(op: ConstantOp) -> Self::T {
         <Self::T as crate::num::Zero>::zero()
+    }
+
+    #[inline(always)]
+    #[allow(unused_variables)]
+    fn base() -> Self::T {
+        <Self::T as crate::num::One>::one()
+    }
+
+    #[inline(always)]
+    #[allow(unused_variables)]
+    fn scale() -> Self::T {
+        <Self::T as crate::num::One>::one()
+    }
+
+    #[inline(always)]
+    fn into_linear(x: V) -> V {
+        x
+    }
+
+    #[inline(always)]
+    fn from_linear(x: V) -> V {
+        x
     }
 
     /// Instance [conversion factor](https://jcgm.bipm.org/vim/en/1.24.html).
@@ -457,6 +480,16 @@ pub trait ConversionFactor<V>:
 {
     /// Raises a `ConversionFactor<V>` to an integer power.
     fn powi(self, e: i32) -> Self;
+
+    /// Raises a `ConversionFactor<V>` to a power.
+    fn pow(self, v: V) -> V {
+        unimplemented!()
+    }
+
+    /// Takes the log_`ConversionFactor<V>` of a value.
+    fn log(self, v: V) -> V {
+        unimplemented!()
+    }
 
     /// Converts a `ConversionFactor<V>` into its underlying storage type.
     fn value(self) -> V;
@@ -525,6 +558,15 @@ storage_types! {
         #[inline(always)]
         fn value(self) -> V {
             self
+        }
+
+        fn pow(self, v: V) -> V {
+            <V as crate::num::Float>::powf(self, v)
+        }
+
+        /// Takes the log_`ConversionFactor<V>` of a value.
+        fn log(self, v: V) -> V {
+            <V as crate::num::Float>::log(self, v)
         }
     }
 
